d63fc7851c71cf2c94cdcb21dfd721fa47961a32
DanilchikB <danil1357997@gmail.com>
2020-09-27 22:00:57 +0300

    added tables and development mode

--- web/web.php
+++ web/web.php
@@ -14,8 +14,16 @@ use Core\Controller;
 use Core\Setting;
 
 Setting::initSetting();
+Router::getNowUrl();
+
 $controller = new Controller();
 $controller->initControllers(Setting::$app['path_app'].'/app/controller/');
 
 
+if(Setting::$app['isPHPDevelopmentServer']){
+    if(Router::checkStaticFile()){
+        //echo 'yes';
+        return false;
+    }
+}
 Router::runController();
